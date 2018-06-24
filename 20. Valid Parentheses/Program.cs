4dbf2dc984bf503d9c04cfc2ca8cfa46f1ae5269
jsw7524 <jsw7524@gmail.com>
2018-06-24 23:42:53 +0800

    AC

--- 20. Valid Parentheses/Program.cs
+++ 20. Valid Parentheses/Program.cs
@@ -6,6 +6,34 @@ using System.Threading.Tasks;
 
 namespace _20.Valid_Parentheses
 {
+
+    public class Solution
+    {
+        Dictionary<char, char> dic = new Dictionary<char, char>() { { ')', '(' }, { '}', '{' }, { ']', '[' } };
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (c == '[' || c == '{' || c=='(')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0 || stack.Peek() != dic[c])
+                    {
+                        return false;
+                    }
+                    stack.Pop();
+                }
+            }
+            if (stack.Count > 0)
+                return false;
+            return true;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _20.Valid_Parentheses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+            var result = sln.IsValid("()");
+            Assert.AreEqual(true,result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+            var result = sln.IsValid("]");
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution sln = new Solution();
+            var result = sln.IsValid("");
+            Assert.AreEqual(true, result);
+        }
+    }
+
+
+}
